ebaefa4817062890ad2cc9081e31d88b0ae55fd5
tentrees <tentreescantbeatme@gmail.com>
2018-11-22 01:19:23 +0100

    Update start and status command

--- lowrmt/src/commands/commands/start.ts
+++ lowrmt/src/commands/commands/start.ts
@@ -1,7 +1,13 @@
 import fs = require('fs-extra');
 import { RawConfig } from '../../config';
 import inquirer = require('inquirer');
-import { InitOptions, StatusOptions, StartOptions, UpdateOptions, StopOptions } from '../../args';
+import {
+  InitOptions,
+  StatusOptions,
+  StartOptions,
+  UpdateOptions,
+  StopOptions
+} from '../../args';
 import { isUndefined } from 'util';
 import { extname } from 'path';
 import replaceExt = require('replace-ext');
@@ -20,44 +26,51 @@ const prompt = inquirer.createPromptModule();
 
 @injectable()
 export class StartCommand extends Command {
+  constructor(
+    @inject(LOWTYPES.Options) private options: StartOptions,
+    @inject(TYPES.HttpApiService) private httpApiService: HttpApiService
+  ) {
+    super('start');
+  }
 
-    constructor(
-        @inject(LOWTYPES.Options) private options: StartOptions,
-        @inject(TYPES.HttpApiService) private httpApiService: HttpApiService
-    ) {
-        super('start');
-    }
-
-    async run() {
-        const { file, force } = this.options;
-        let result = await this.httpApiService.Start({ action: 'start', file });
-        switch (result) {
-            case 'FILE_NOT_FOUND':
-                throw new RunError('The file to start does not exist.');
-            case 'ALREADY_RUNNING':
-                let doRestart=false;
-                if (force) {
-                    doRestart=true;
-                } else {
-                    const { restart } = await prompt<{restart:boolean}>({
-                        name: 'restart',
-                        type: 'confirm',
-                        message: 'The user application is already running. Restart? (Use the --force option in the future to skip this prompt and force a restart.)',
-                        default: true,
-                    });
-                    doRestart=restart;
-                }
-                if (doRestart){
-                    await this.httpApiService.Stop();
-                    const result = await this.httpApiService.Start({ action: 'start', file });
-                    switch (result) {
-                        case 'FILE_NOT_FOUND':
-                            throw new RunError('The file to start does not exist.');
-                        case 'ALREADY_RUNNING':
-                            throw new Error('Could not restart program.');
-                    }
-                }
-                break;
+  async run() {
+    const { file, force } = this.options;
+    let result = await this.httpApiService.Start({ action: 'start', file });
+    switch (result) {
+      case 'UPDATING_SYS':
+        throw new RunError(
+          'Cannot start the program, because the microcontroller is currently performing a system update.'
+        );
+      case 'FILE_NOT_FOUND':
+        throw new RunError('The file to start does not exist.');
+      case 'ALREADY_RUNNING':
+        let doRestart = false;
+        if (force) {
+          doRestart = true;
+        } else {
+          const { restart } = await prompt<{ restart: boolean }>({
+            name: 'restart',
+            type: 'confirm',
+            message:
+              'The user application is already running. Restart? (Use the --force option in the future to skip this prompt and force a restart.)',
+            default: true
+          });
+          doRestart = restart;
         }
+        if (doRestart) {
+          await this.httpApiService.Stop();
+          const result = await this.httpApiService.Start({
+            action: 'start',
+            file
+          });
+          switch (result) {
+            case 'FILE_NOT_FOUND':
+              throw new RunError('The file to start does not exist.');
+            case 'ALREADY_RUNNING':
+              throw new Error('Could not restart program.');
+          }
+        }
+        break;
     }
+  }
 }
--- lowrmt/src/commands/commands/status.ts
+++ lowrmt/src/commands/commands/status.ts
@@ -1,30 +1,34 @@
-import fs = require('fs-extra');
-import { RawConfig } from '../../config';
-import inquirer = require('inquirer');
-import { InitOptions, StatusOptions, StartOptions, UpdateOptions, StopOptions } from '../../args';
-import { isUndefined } from 'util';
-import { extname } from 'path';
-import replaceExt = require('replace-ext');
-import { ArgumentOutOfRangeError } from 'rxjs';
-import { RunError } from '../../runError';
-import { Command } from '../command';
-import { injectable, inject } from 'inversify';
-import * as prettyjson from 'prettyjson';
-import { TYPES } from '@common/src/types';
 import { HttpApiService } from '@common/src/services/http/api';
+import { TYPES } from '@common/src/types';
+import { inject, injectable } from 'inversify';
+import { Command } from '../command';
 
 @injectable()
 export class StatusCommand extends Command {
+  constructor(
+    @inject(TYPES.HttpApiService) private httpApiService: HttpApiService
+  ) {
+    super('status');
+  }
 
-    constructor(
-        @inject(TYPES.HttpApiService) private httpApiService: HttpApiService
-    ) { 
-        super('status');
-    }
+  async run() {
+    const {
+      code: { status }
+    } = await this.httpApiService.Status({ code: true });
 
-    async run() {
-        const { code: { status } } = await this.httpApiService.Status({ code: true });
-        console.log(status);
+    let statusStr: string;
+
+    switch (status) {
+      case 'paused':
+        statusStr = 'paused / crashed';
+        break;
+      case 'updating_sys':
+        statusStr = 'performing system update';
+        break;
+      default:
+        statusStr = status;
+        break;
     }
+    console.log(`Current status: ${statusStr}`);
+  }
 }
-
