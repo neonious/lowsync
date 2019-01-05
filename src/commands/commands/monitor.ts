2c1766426ca732ad9166b1cee14370be85c4a7ec
tentrees <tentrees@users.noreply.github.com>
2019-01-05 01:50:15 +0100

    Make monitor output a bit better

--- src/commands/commands/monitor.ts
+++ src/commands/commands/monitor.ts
@@ -1,8 +1,12 @@
 import { ConsoleMessage } from '@common/services/consoleMessage/message';
 import chalk from 'chalk';
 import { Command } from '../command';
-import { format } from '../../../common/src/services/consoleMessage/formatter';
+import {
+  format,
+  getTimestampPrefix
+} from '../../../common/src/services/consoleMessage/formatter';
 import { getConsoleMessages } from '../../../common/src/services/consoleMessage/messages';
+import { padStart } from 'lodash';
 
 export default class MonitorCommand extends Command {
   readonly requestConfig = {};
@@ -13,26 +17,41 @@ export default class MonitorCommand extends Command {
   }
 
   private writeConsole({ timestamp, level, lines }: ConsoleMessage) {
-    const line = format(timestamp, lines);
-    switch (level) {
-      case 'd':
-        console.log(chalk.gray(line));
-        break;
-      case 'l':
-        console.log(line);
-        break;
-      case 'w':
-        console.log(chalk.keyword('orange')(line));
-        break;
-      case 'e':
-        console.log(chalk.red(line));
-        break;
-      default:
-        throw new Error('Unknown log level: ' + level);
+    const prefix = getTimestampPrefix(timestamp);
+    lines = Array.isArray(lines) ? lines : [lines];
+    lines = lines.map((line, i) => {
+      let trline;
+
+      switch (level) {
+        case 'd':
+          trline = chalk.gray(line);
+          break;
+        case 'l':
+          trline = line;
+          break;
+        case 'w':
+          trline = chalk.keyword('orange')(line);
+          break;
+        case 'e':
+          trline = chalk.red(line);
+          break;
+        default:
+          throw new Error('Unknown log level: ' + level);
+      }
+
+      if (i === 0) {
+        return `${prefix}${trline}`;
+      }
+      return padStart(trline, prefix.length + trline.length);
+    });
+
+    for (const line of lines) {
+      console.log(line);
     }
   }
 
   async run() {
+    console.log(chalk.bold('Retrieving microcontroller output...'));
     getConsoleMessages().subscribe(msg => {
       this.writeConsole(msg);
     });
