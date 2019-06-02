6cfb57f38654b3d22dca0eaa615132b77a496ac8
Thomas Rogg <thomas@neonious.com>
2019-06-02 21:50:30 +0200

    uploaded small changes

--- src/commands/commands/flash.ts
+++ src/commands/commands/flash.ts
@@ -7,7 +7,6 @@ import * as os from 'os';
 import * as path from 'path';
 import { FlashOptions } from '../../args';
 import { RunError } from '../../runError';
-import { resolveCname } from 'dns';
 
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
